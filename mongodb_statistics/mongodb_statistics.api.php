a4c54abb275aa2a5b88e988d4995648ba8ab4a3a
Régis Leroy <rle@makina-corpus.com>
2012-03-16 16:00:58 +0100

    CHG: hook to add custom reports on node's stat tab

--- mongodb_statistics/mongodb_statistics.api.php
+++ mongodb_statistics/mongodb_statistics.api.php
@@ -32,3 +32,31 @@ function hook_mongodb_statistics_firstvisits($visits) {
   }
   
 }
+
+/**
+ * hook_mongodb_statistics_node_report
+ *
+ * Hook used to render a report on the node statistics tab
+ * 
+ * WARNING: returned boolean is important!
+ * 
+ * We are giving you the current node, and the current array of content rendered on this tab.
+ * 
+ * @param $node: the current node
+ * @param: $build: the current render array of this tab 
+ * @return boolean: TRUE if you had anything to report FALSE instead
+ */
+function hook_mongodb_statistics_node_report($node,&$build) {
+  if (!$data = my_custom_stats_data($node->nid)) {
+    return FALSE;
+  }
+  $build['custom_statistics_report_title'] = array(
+    '#type' => 'markup',
+    '#markup' => '<h3>'.t('My custom report.').'</h3>',
+  );
+  $build['custom_statistics_report_table'] = array(
+    '#type' => 'markup',
+    '#markup' => '<div>'.t('Here I should build a table with $data.').'</div>',
+  );
+  return TRUE;
+}
